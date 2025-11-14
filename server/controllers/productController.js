f05ee7f731cb4117e7bd289899d5343217a5b1ad
ATULYADAVpro <atul088793151@gmail.com>
2025-11-14 14:09:32 +0530

    add product schema and set up controller

---
+++ server/controllers/productController.js
@@ -0,0 +1,21 @@
+
+
+// add product 
+export const addProduct = async (req, res) => {
+
+}
+
+// get product 
+export const productList = async (req, res) => {
+
+}
+
+// get single product 
+export const productById = async (req, res) => {
+
+}
+
+// change product inStock
+export const changeStock = async (req, res) => {
+
+}
---
+++ server/models/Product.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose'
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: Array,
+        required: true
+    },
+    image: {
+        type: Array,
+        required: true
+    },
+    category: {
+        type: Array,
+        required: true
+    },
+    pric: {
+        type: Number,
+        required: true
+    },
+    offerPrice: {
+        type: Number,
+        required: true
+    },
+    inStock: {
+        type: Boolean,
+        default: true,
+    },
+}, { timestamps: true })
+
+const Product = model('product', productSchema)
+
+export default Product
