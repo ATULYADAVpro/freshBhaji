bf4c45221d68bde229041c26727eb967e431f5a3
ATULYADAVpro <atul088793151@gmail.com>
2025-11-14 20:58:43 +0530

    add order controller and router

---
+++ server/models/Order.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose'
+
+const orderSchema = new Schema({
+    userId: { type: String, required: true, ref: 'user' },
+    items: [{
+        product: { type: String, required: true, ref: 'product' },
+        quantity: { type: Number, required: true }
+    }],
+    amount: { type: Number, required: true },
+    address: { type: String, required: true, ref: 'address' },
+    status: { type: String, default: 'Order Placed' },
+    paymentType: { type: String, required: true },
+    isPaid: { type: Boolean, required: true, default: false },
+}, { timestamps: true })
+
+const Order = model('order', orderSchema)
+
+export default Order;
